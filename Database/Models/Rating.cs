12537e66b444a458b20d56fadd410db5a8c01136
danylokl <user>
2022-08-06 05:02:51 +0300

    Add project files.

---
+++ Database/Models/Rating.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+       
+        public virtual Book BookId { get; set; }
+        public double Score { get; set; }
+
+    }
+}
---
+++ Task2-Radency-Internship/Program.cs
@@ -0,0 +1,31 @@
+using Database;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+namespace Task2_Radency_Internship
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<Context>(options=>
+            options.UseInMemoryDatabase(databaseName:"Library"));
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
